dbd5dfeea41e45fab325dd8b73ba7bbefd8ed450
Lucy <43696299+lucy70381@users.noreply.github.com>
2023-05-22 21:58:51 +0800

    feat: adjust header style

--- src/layout/components/header.tsx
+++ src/layout/components/header.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <div>
-      <nav className='flex justify-between py-5'>
-        <Link to='/'>
-          <h1 className=' block overflow-hidden w-[147px] md:w-[244px] h-6 md:h-10 bg-logo indent-[101%] bg-contain bg-no-repeat'>
+      <nav className="flex items-center justify-between py-5">
+        <Link to="/">
+          <h1 className="block h-6 w-[147px] overflow-hidden whitespace-nowrap bg-logo bg-contain bg-no-repeat indent-[101%] md:h-10 md:w-[244px]">
             AI 工具王
           </h1>
         </Link>
-        <ul className='flex gap-5'>
-          <li>
-            <Link to='/2'>首頁</Link>
+        <ul className="flex gap-5">
+          <li key="home">
+            <Link to="/">首頁</Link>
           </li>
-          <li>
-            <Link to=''>定價</Link>
+          <li key="price">
+            <Link to="">定價</Link>
           </li>
         </ul>
       </nav>
