e0ed6ddce3ad49ab1fa371b3cc1fada88d62983b
Lucy <lucy70381@kimo.com>
2023-05-28 20:28:22 +0800

    style: add font bold

--- src/layout/components/header.tsx
+++ src/layout/components/header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <div>
-      <nav className="flex items-center justify-between py-5">
+      <nav className="flex items-center justify-between py-5 font-bold">
         <Link to="/">
           <h1 className="block h-6 w-[147px] overflow-hidden whitespace-nowrap bg-logo bg-contain bg-no-repeat indent-[101%] md:h-10 md:w-[244px]">
             AI 工具王
@@ -14,7 +14,7 @@ export const Header = () => {
             <Link to="/">首頁</Link>
           </li>
           <li key="price">
-            <Link to="">定價</Link>
+            <Link to="/price">定價</Link>
           </li>
         </ul>
       </nav>
